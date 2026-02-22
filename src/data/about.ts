0288d089824ef20afeecdcf50be4d4b913b29579
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2026-02-22 21:57:11 +0530

    update data

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,7 +1,5 @@
 export const aboutBioData = `
-I'm passionate about Full-Stack Web Development, DevOps, Web Security,
-Containerization, Self-Hosting. In my free time, I make Open-Source
-CLI Tools, Web Apps & Discord Bots, which anyone can self-host or use
-the hosted version. I also contribute to Open-Source softwares
-sometimes.
+		A Full-Stack Developer with a strong focus on DevOps, Web Security, and Self-Hosting. Dedicated to 
+    building Open-Source CLI Tools \& Web Apps designed for self-hosted and hosted environments, with 
+    occasional contributions to other projects. Leads technical initiatives within the student developer community.
 `;
